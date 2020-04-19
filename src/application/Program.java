b6d9d977a0da86ee0de4a323e40be3d36a620139
MichaelMaiello <michael_maiello@hotmail.com>
2020-04-19 16:15:21 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,10 +35,14 @@ public class Program {
 		}
 		
 		System.out.println("\n=== TEST 4: seller insert =====");
-
 		Seller newSeller = new Seller(null, "Ze", "Ze@com.br", new Date(), 4000.0, department);
-		
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New "+ newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller Update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 	}
 }
